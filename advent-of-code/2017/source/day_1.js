f08570bebe3e93905d1ade1c4a701f475f5ce16b
Landon Schropp <schroppl@gmail.com>
2017-12-02 15:21:56 -0800

    Refactor day 1 implementation
    Add an explicit lookahead count, and break everything apart into
    separate functions.

--- advent-of-code/2017/source/day_1.js
+++ advent-of-code/2017/source/day_1.js
@@ -1,17 +1,30 @@
 import * as R from 'ramda';
 
-// String -> Integer
-export const part1 = R.pipe(
+// Appends the lookahead digits from the start of the string to the end.
+// Number -> String -> String
+const extendInput = R.curry((lookahead, input) => {
+  return R.concat(input, input.slice(0, lookahead))
+});
 
-  // Instead of mucking around with complicated regular expressions, simply duplicate the last
-  // character.
-  R.converge(R.concat, [ R.identity, R.head ]),
+// Removes all non-repeating characters (using a lookahead) from an extended input.
+// Number -> String -> String
+const removeNonRepeating = R.curry((lookahead, input) => {
+  return R.replace(RegExp(`(.).{${ lookahead - 1 }}(?!\\1)`, "g"), "", input);
+});
 
-  // Remove all non-repeating characters
-  R.replace(/(.)(?!\1)/g, ""),
-
-  // Parse the remaining digits and add them together
+// Add all of the digits in the provided string together.
+// String -> Number
+const addDigits = R.pipe(
   R.split(""),
   R.map(R.curryN(2, parseInt)(R.__, 10)),
   R.reduce(R.add, 0)
 );
+
+// String -> Integer
+export const part1 = R.pipe(
+  extendInput(1),
+  removeNonRepeating(1),
+  addDigits
+);
+
+export const part2 = R.identity;
