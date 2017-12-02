ec322546e5fcb737763d63ec4285d35ac5208a6c
Landon Schropp <schroppl@gmail.com>
2017-12-02 15:33:31 -0800

    Solve day 1 part 2

--- advent-of-code/2017/source/day_1.js
+++ advent-of-code/2017/source/day_1.js
@@ -3,13 +3,13 @@ import * as R from 'ramda';
 // Appends the lookahead digits from the start of the string to the end.
 // Number -> String -> String
 const extendInput = R.curry((lookahead, input) => {
-  return R.concat(input, input.slice(0, lookahead))
+  return R.concat(input, input.slice(0, lookahead));
 });
 
 // Removes all non-repeating characters (using a lookahead) from an extended input.
 // Number -> String -> String
 const removeNonRepeating = R.curry((lookahead, input) => {
-  return R.replace(RegExp(`(.).{${ lookahead - 1 }}(?!\\1)`, "g"), "", input);
+  return R.replace(RegExp(`(.)(?!.{${ lookahead - 1 }}\\1)`, "g"), "", input);
 });
 
 // Add all of the digits in the provided string together.
@@ -20,11 +20,18 @@ const addDigits = R.pipe(
   R.reduce(R.add, 0)
 );
 
-// String -> Integer
-export const part1 = R.pipe(
-  extendInput(1),
-  removeNonRepeating(1),
-  addDigits
-);
+// Solve the puzzle using the given lookahead number and input.
+// Integer -> String -> Integer
+const puzzle = R.curry((lookahead, input) => {
+  return R.pipe(
+    extendInput(lookahead),
+    removeNonRepeating(lookahead),
+    addDigits
+  )(input);
+});
 
-export const part2 = R.identity;
+// String -> Integer
+export const part1 = puzzle(1);
+
+// String -> Integer
+export const part2 = input => puzzle(input.length / 2, input);
--- advent-of-code/2017/spec/day_1_spec.js
+++ advent-of-code/2017/spec/day_1_spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { part1 } from '../source/day_1';
+import { part1, part2 } from '../source/day_1';
 
 describe("part1", () => {
 
@@ -11,6 +11,20 @@ describe("part1", () => {
       expect(part1("1111")).to.equal(4);
       expect(part1("1234")).to.equal(0);
       expect(part1("91212129")).to.equal(9);
+    });
+  });
+});
+
+describe("part2", () => {
+
+  context("when the sample inputs are provided", () => {
+
+    it("returns the provided solutions", () => {
+      expect(part2("1212")).to.equal(6);
+      expect(part2("1221")).to.equal(0);
+      expect(part2("123425")).to.equal(4);
+      expect(part2("123123")).to.equal(12);
+      expect(part2("12131415")).to.equal(4);
     });
   });
 });
