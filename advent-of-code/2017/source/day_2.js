d493d6c26914cd941b24dad0bf7c23e084c35320
Landon Schropp <schroppl@gmail.com>
2017-12-02 16:26:04 -0800

    Day 2 part 2

--- advent-of-code/2017/source/day_2.js
+++ advent-of-code/2017/source/day_2.js
@@ -8,12 +8,31 @@ export const parse = R.pipe(
   R.map(R.map(parseInt))
 );
 
-// Determine the row's checksum (min + max)
-// [ Integer ] -> Integer
-const rowChecksum = R.converge(R.subtract, [ R.apply(Math.max), R.apply(Math.min) ]);
+// Returns the max and min values of an array.
+// [ Integer ] -> [ Integer ]
+const maxAndMin = R.converge(R.unapply(R.identity), [ R.apply(Math.max), R.apply(Math.min) ]);
+
+// Determines if the first number is evenly divisible by the second number.
+const isEvenlyDivisible = (first, second) => first !== second && first % second === 0;
+
+// Returns all of the possible pairings of two arrays.
+// [ * ] -> [ * ] -> [ [ *, * ] ]
+function pairs(numbers) {
+  return R.unnest(R.map(x => R.map(y => [ x, y ], numbers), numbers));
+}
+
+// Returns the evenly divisible numbers in an array.
+// NOTE: This function could easily be made more efficint, but who cares. ¯\_(ツ)_/¯
+const evenlyDivisibleNumbers = R.pipe(pairs, R.find(R.apply(isEvenlyDivisible)));
 
 export const part1 = R.pipe(
   parse,
-  R.map(rowChecksum),
+  R.map(R.pipe(maxAndMin, R.apply(R.subtract))),
+  R.reduce(R.add, 0)
+);
+
+export const part2 = R.pipe(
+  parse,
+  R.map(R.pipe(evenlyDivisibleNumbers, R.apply(R.divide))),
   R.reduce(R.add, 0)
 );
--- advent-of-code/2017/spec/day_2_spec.js
+++ advent-of-code/2017/spec/day_2_spec.js
@@ -1,13 +1,23 @@
 import { expect } from 'chai';
 
-import { part1 } from '../source/day_2';
+import { part1, part2 } from '../source/day_2';
 
-describe.only("part1", () => {
+describe("part1", () => {
 
   context("when the sample input is provided", () => {
 
     it("returns the expected result", () => {
       expect(part1("5 1 9 5\n7 5 3\n2 4 6 8")).to.eq(18);
+    });
+  });
+});
+
+describe("part2", () => {
+
+  context("when the sample input is provided", () => {
+
+    it("returns the expected result", () => {
+      expect(part2("5 9 2 8\n9 4 7 3\n3 8 6 5")).to.eq(9);
     });
   });
 });
