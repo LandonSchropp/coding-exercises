13b7b91fc8fde9ca123d2326f60f562c8cafd5db
Landon Schropp <schroppl@gmail.com>
2019-07-09 17:06:16 -0700

    Implement Hamming problem

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+import _ from 'lodash';
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (left, right) => {
+  if (left.length === 0 && right.length > 0) { throw "left strand must not be empty"; }
+  if (right.length === 0 && left.length > 0) { throw "right strand must not be empty"; }
+  if (right.length !== left.length) { throw "left and right strands must be of equal length"; }
+
+  return _.sum(_.zipWith(_.split(left, ''), _.split(right, ''), (a, b) => a === b ? 0 : 1));
 };
