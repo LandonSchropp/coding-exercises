c7d4e51f8f80fac455419c11fc76355bc592254a
Landon Schropp <schroppl@gmail.com>
2019-07-17 12:22:51 -0700

    Genericize the accumulate solution

--- typescript/accumulate/accumulate.ts
+++ typescript/accumulate/accumulate.ts
@@ -1,11 +1,11 @@
 // I'm cheating the problem description here a bit to show how all of the Lodash array
 // transformation functions can be rewritten using reduce.
-export default (array: any[], iteratee: (element: any) => any) => {
+export default <E>(array: E[], iteratee: (element: E) => any) => {
   return array.reduce(
     (result, element) => {
       result.push(iteratee(element))
       return result
     },
-    []
+    [] as E[]
   )
 }
