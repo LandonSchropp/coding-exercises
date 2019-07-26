a539912693284dfc857d25fcbc9f78423a0134bc
Landon Schropp <schroppl@gmail.com>
2019-07-25 17:37:02 -0700

    Implement Nucleotide Count problem

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,6 +1,12 @@
+import _ from "lodash";
+
 export class NucleotideCounts {
 
   static parse(dna) {
+    if (!/^[ACGT]*$/.test(dna)) {
+      throw new Error("Invalid nucleotide in strand");
+    }
 
+    return _.values({ A: 0, C: 0, G: 0, T: 0, ..._.countBy(dna.split('')) }).join(" ");
   }
 }
