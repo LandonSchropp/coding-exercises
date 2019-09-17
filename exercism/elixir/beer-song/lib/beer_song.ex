014946cd3929b2488699ca2e82bbc0e93abc68dd
Landon Schropp <schroppl@gmail.com>
2019-09-16 17:44:17 -0700

    Implement beer song

--- exercism/elixir/beer-song/lib/beer_song.ex
+++ exercism/elixir/beer-song/lib/beer_song.ex
@@ -1,17 +1,46 @@
+# NOTE: The original problem passes a range to the `lyrics` function. I wanted to practice
+# recursion, so I'm changing that to an integer.
+
 defmodule BeerSong do
+
   @doc """
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    # Your implementation here...
+    """
+    #{ bottles(number) } of beer on the wall, #{ bottles(number) } of beer.
+    Take one down and pass it around, #{ bottles(number - 1) } of beer on the wall.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
-  @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    # Your implementation here...
+  @spec lyrics(integer) :: String.t()
+  def lyrics(0), do: verse(0)
+
+  def lyrics(number) do
+    verse(number) <> "\n" <> lyrics(number - 1)
   end
+
+  @doc """
+  Pluralizes the word "bottles" based on the provided number.
+  """
+  defp bottles(1), do: "1 bottle"
+  defp bottles(number), do: "#{ number } bottles"
 end
--- exercism/elixir/beer-song/test/beer_song_test.exs
+++ exercism/elixir/beer-song/test/beer_song_test.exs
@@ -30,7 +30,7 @@ defmodule BeerSongTest do
   end
 
   test "getting the last 4 verses" do
-    assert BeerSong.lyrics(3..0) == """
+    assert BeerSong.lyrics(3) == """
            3 bottles of beer on the wall, 3 bottles of beer.
            Take one down and pass it around, 2 bottles of beer on the wall.
 
@@ -46,7 +46,7 @@ defmodule BeerSongTest do
   end
 
   test "getting the whole song" do
-    assert BeerSong.lyrics() == """
+    assert BeerSong.lyrics(99) == """
            99 bottles of beer on the wall, 99 bottles of beer.
            Take one down and pass it around, 98 bottles of beer on the wall.
 
