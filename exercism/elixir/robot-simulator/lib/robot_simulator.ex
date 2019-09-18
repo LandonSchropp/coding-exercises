5d1f1469ce6efefaea35d938e9907ac4bfdfae11
Landon Schropp <schroppl@gmail.com>
2019-09-18 02:34:21 -0700

    Initial implementation of robot simulator

--- exercism/elixir/robot-simulator/lib/robot_simulator.ex
+++ exercism/elixir/robot-simulator/lib/robot_simulator.ex
@@ -1,11 +1,59 @@
 defmodule RobotSimulator do
+
+  @directions [ :north, :east, :south, :west ]
+
+  @invalid_direction { :error, "invalid direction" }
+  @invalid_position { :error, "invalid position" }
+  @invalid_instruction { :error, "invalid instruction" }
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(direction \\ nil, position \\ nil)
+
+  # Validate the direction and position.
+  def create(direction, _) when direction not in @directions, do: @invalid_direction
+  def create(_, position) when not is_tuple(position), do: @invalid_position
+  def create(_, position) when tuple_size(position) != 2, do: @invalid_position
+  def create(_, { x, _ }) when not is_integer(x), do: @invalid_position
+  def create(_, { _, y }) when not is_integer(y), do: @invalid_position
+
+  # Represent the robot as a tuple.
+  def create(direction, position), do: { direction, position }
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction({ direction, _ }), do: direction
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position({ _, position }), do: position
+
+  @doc """
+  Turns a robot to the right.
+  """
+  def turn_right({ direction, position }), do: { rotate(direction, 1), position }
+
+  @doc """
+  Turns a robot to the left.
+  """
+  def turn_left({ direction, position }), do: { rotate(direction, -1), position }
+
+  @doc """
+  Advances a robot forward one space.
+  """
+  def advance({ direction, { x, y } }) do
+    { vector_x, vector_y } = direction_to_vector(direction)
+    { direction, { x + vector_x, y + vector_y } }
   end
 
   @doc """
@@ -14,22 +62,51 @@ defmodule RobotSimulator do
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
-  def simulate(robot, instructions) do
+
+  # Sanitization: Validate the instructions and break them into a list.
+  def simulate(robot, instructions) when is_binary(instructions) do
+    # if String.match? instructions, ~r/[^LRA]/
+    #   @invalid_instruction
+    # else
+    #   simulate(robot, String.graphemes(instructions))
+    # end
   end
 
-  @doc """
-  Return the robot's direction.
+  # Base case: There are no more instructions.
+  def simulate(robot, []), do: robot
 
-  Valid directions are: `:north`, `:east`, `:south`, `:west`
-  """
-  @spec direction(robot :: any) :: atom
-  def direction(robot) do
+  # Recursive cases: The robot moves left, the robot moves right or the robot advances.
+  def simulate(robot, [ "L" | tail ]), do: simulate(turn_left(robot), tail)
+  def simulate(robot, [ "R" | tail ]), do: simulate(turn_right(robot), tail)
+  def simulate(robot, [ "A" | tail ]), do: simulate(advance(robot), tail)
+
+  # Converts a direction to its index in the directions tuple.
+  defp direction_to_index(direction) do
+    Enum.find_index(@directions, fn element -> element == direction end)
   end
 
-  @doc """
-  Return the robot's position.
-  """
-  @spec position(robot :: any) :: {integer, integer}
-  def position(robot) do
+  # Converts an index to a direction.
+  defp index_to_direction(index) do
+    elem(@directions, Integer.mod(index, length(@directions)))
+  end
+
+  # Rotates a direction by the given number of turns. Positive numbers represent right turns and
+  # negative numbers represent left turns.
+  defp rotate(direction, turns) do
+    direction
+    |> direction_to_index
+    |> (fn index -> index + turns end).()
+    |> index_to_direction
+  end
+
+  # Converts a direction index to a vector.
+  defp direction_to_vector(direction) do
+    index = direction_to_index(direction)
+
+    oscillate = fn index ->
+      rem(index, 2) + div(index, 2) * -2
+    end
+
+    { oscillate.(index), oscillate.(Integer.mod(index - 1, 4)) }
   end
 end
