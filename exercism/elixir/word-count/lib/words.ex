59da035cd0d24bc2ccbae02426e0632efebe61b6
Landon Schropp <schroppl@gmail.com>
2019-09-18 01:05:09 -0700

    Solve the word count problem

--- exercism/elixir/word-count/lib/words.ex
+++ exercism/elixir/word-count/lib/words.ex
@@ -5,6 +5,15 @@ defmodule Words do
   Words are compared case-insensitively.
   """
   @spec count(String.t()) :: map
-  def count(sentence) do
+
+  # Sanitization: Split the string into a list of words and pipe it into the recursive count.
+  def count(sentence) when is_binary(sentence) do
+    sentence |> String.downcase |> String.split(~r/[^[:alpha:]\d-]+/u, trim: true) |> count
   end
+
+  # Base case: The list is empty.
+  def count([]), do: %{}
+
+  # Recursive case: Update the current value of the item by 1, or set it to 1 if not yet present.
+  def count([ head | tail ]), do: Map.update(count(tail), head, 1, fn value -> value + 1 end)
 end
