52c1fc3d819c80d07de92397cf53b32f7edb0a6c
Landon Schropp <schroppl@gmail.com>
2019-09-18 00:30:24 -0700

    Implement space age

--- exercism/elixir/rna-transcription/lib/rna_transcription.ex
+++ exercism/elixir/rna-transcription/lib/rna_transcription.ex
@@ -1,13 +1,11 @@
 defmodule RnaTranscription do
+
+  @nucleotide_pairs %{ ?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U }
+
   @doc """
-  Transcribes a character list representing DNA nucleotides to RNA
-
-  ## Examples
-
-  iex> RnaTranscription.to_rna('ACTG')
-  'UGAC'
+  Transcribes a character list representing DNA nucleotides to RNA.
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-  end
+  def to_rna([]), do: []
+  def to_rna([ head | tail ]), do: [ @nucleotide_pairs[head] | to_rna(tail) ]
 end
