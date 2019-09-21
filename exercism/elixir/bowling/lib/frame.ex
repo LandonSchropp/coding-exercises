1925be6f90a90cb833d5266ecf628f99c5ebb809
Landon Schropp <schroppl@gmail.com>
2019-09-20 23:12:55 -0700

    Add Frame.strike?/1 and Frame.spare?/1

--- exercism/elixir/bowling/lib/frame.ex
+++ exercism/elixir/bowling/lib/frame.ex
@@ -16,4 +16,11 @@ defmodule Frame do
   def chunk([ head ]), do: [ [ head ] ]
   def chunk([ 10 | tail ]), do: [ [ 10 ] | chunk(tail) ]
   def chunk([ first | [ second | tail ] ]), do: [ [ first, second ] | chunk(tail) ]
+
+  def strike?([ 10 ]), do: true
+  def strike?(_), do: false
+
+  def spare?([]), do: false
+  def spare?([ _ ]), do: false
+  def spare?([ first_roll, second_roll ]), do: first_roll + second_roll == 10
 end
--- exercism/elixir/bowling/test/frame_test.exs
+++ exercism/elixir/bowling/test/frame_test.exs
@@ -1,6 +1,26 @@
 defmodule FrameTest do
   use ExUnit.Case
 
+  # %{ score: 43, active_strikes: 1, active_spares: 1, frame: 1, frame_complete: false }
+  describe "complete?" do
+
+    test "returns true when when the frame is a strike" do
+      assert Frame.complete? [ 10 ]
+    end
+
+    test "returns true when the frame has two rolls" do
+      assert Frame.complete? [ 5, 4 ]
+    end
+
+    test "returns false when the frame has one roll" do
+      refute Frame.complete? [ 5 ]
+    end
+
+    test "returns false when the frame has no rolls" do
+      refute Frame.complete? []
+    end
+  end
+
   describe "chunk" do
 
     test "returns an empty list when the rolls list is empty" do
@@ -35,6 +55,51 @@ defmodule FrameTest do
         [ 10 ],
         [ 0 ]
       ]
+    end
+  end
+
+  describe "strike?" do
+
+    test "returns false when the frame is empty" do
+      refute Frame.strike?([])
+    end
+
+    test "returns true when the frame contains only one roll of ten" do
+      assert Frame.strike?([ 10 ])
+    end
+
+    test "returns false when the frame contains one value that's not 10" do
+      refute Frame.strike?([ 9 ])
+      refute Frame.strike?([ 0 ])
+    end
+
+    test "returns false when the frame contains two values" do
+      refute Frame.strike?([ 0, 10 ])
+      refute Frame.strike?([ 9, 1 ])
+    end
+  end
+
+  describe "spare?" do
+
+    test "returns false when the frame is empty" do
+      refute Frame.spare?([])
+    end
+
+    test "returns false when the frame only contains one value" do
+      refute Frame.spare?([ 0 ])
+      refute Frame.spare?([ 10 ])
+    end
+
+    test "returns false when the frame does not add up to 10" do
+      refute Frame.spare?([ 0, 0 ])
+      refute Frame.spare?([ 1, 8 ])
+      refute Frame.spare?([ 8, 1 ])
+    end
+
+    test "returns true when the frame adds up to 10" do
+      assert Frame.spare?([ 0, 10 ])
+      assert Frame.spare?([ 9, 1 ])
+      assert Frame.spare?([ 5, 5 ])
     end
   end
 end
