9b7658115e6879b6bbfa7363a48bf533a0227164
Landon Schropp <schroppl@gmail.com>
2019-09-23 00:29:00 -0700

    Parse integers automatically

--- exercism/elixir/forth/lib/forth.ex
+++ exercism/elixir/forth/lib/forth.ex
@@ -29,19 +29,13 @@ defmodule Forth do
   """
   @spec eval(evaluator, String.t()) :: evaluator
 
-  @doc """
-  Returns true if the word is known to the evaluator.
-  """
-  def known_word?({ stack, operations }, word) do
-    Enum.member?(Map.keys(operations), word)
-  end
-
   # Parse the string into a list set of operations.
   def eval(evaluator, string) when is_binary(string) do
 
     operations = string
     |> String.downcase
     |> String.split(~r/[^\da-z+*\-\/]+/, trim: true)
+    |> Enum.map(fn word -> parse_if_integer(word) end)
 
     # TODO: Determine why \W doesn't work in the regular expression.
     # TODO: Parse anything that looks like a number into a number so it can be pattern matched.
@@ -51,23 +45,18 @@ defmodule Forth do
   # Base case: When there are no operations, return the evaluator as is.
   def eval(evaluator, []), do: evaluator
 
+  # Recursive case: When the value is a number, add it to the stack.
+  def eval({ stack, operations }, [ number | words ]) when is_number(number) do
+    eval({ [ number | stack ], operations }, words)
+  end
+
   # Recursive case: When there's an operation, evaluate it.
-  def eval(evaluator, [ word | words ]) do
-    cond do
-      known_word?(evaluator, word) -> eval(operate(evaluator, word), words)
-      String.match?(word, ~r/^\d+$/) -> eval(push(evaluator, word), words)
-      true -> raise Error.UnknownWord
+  def eval({ stack, operations }, [ word | words ]) do
+    if Map.has_key?(operations, word) do
+      eval({ operations[word].(stack), operations }, words)
+    else
+      raise Error.UnknownWord
     end
-  end
-
-  # Pushes the number onto the stack in the evaluator.
-  defp push({ stack, operations }, number) do
-    { [ elem(Integer.parse(number), 0) | stack ], operations }
-  end
-
-  # Evaluate operations defined in the words hash.
-  defp operate({ stack, operations }, word) do
-    { operations[word].(stack), operations }
   end
 
   # Adds a word to the evaluator.
@@ -79,5 +68,14 @@ defmodule Forth do
         stack -> operation.(stack)
       end)
     }
+  end
+
+  # Parses the word as an integer, but only if contains only digits.
+  defp parse_if_integer(word) do
+    if String.match? word, ~r/^\d+$/ do
+      elem(Integer.parse(word), 0)
+    else
+      word
+    end
   end
 end
