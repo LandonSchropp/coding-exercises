ba99bab70e82229e9efc4867456f0e71aff77ebf
Landon Schropp <schroppl@gmail.com>
2019-09-22 18:39:07 -0700

    Clean up the evaluator data structure

--- exercism/elixir/forth/lib/forth.ex
+++ exercism/elixir/forth/lib/forth.ex
@@ -1,25 +1,35 @@
 defmodule Forth do
   @opaque evaluator :: any
 
+  # Defines the default set of words that can be used when evaluating input strings.
+  @words {}
+
+  # Iterpertation:
+  #
+  # * If the item is a digit, add it to the stack.
+  # * If the item is in the words map, then apply the item.
+  # * Otherwise, the operation is invalid.
+
   @doc """
   Create a new evaluator.
   """
   @spec new() :: evaluator
-  def new(), do: []
+  def new(), do: { [], @words }
 
   @doc """
   Evaluate an input string, updating the evaluator state.
   """
   @spec eval(evaluator, String.t()) :: evaluator
-  def eval(ev, s) do
+  def eval(evaluator, string) when is_binary(string) do
+    elem(evaluator, 0)
   end
 
   @doc """
-  Return the current stack as a string with the element on top of the stack
-  being the rightmost element in the string.
+  Return the current stack as a string with the element on top of the stack being the rightmost
+  element in the string.
   """
   @spec format_stack(evaluator) :: String.t()
-  def format_stack(stack), do: stack |> Enum.reverse |> Enum.join(" ")
+  def format_stack(evaluator), do: evaluator |> elem(0) |> Enum.reverse |> Enum.join(" ")
 
   defmodule StackUnderflow do
     defexception []
