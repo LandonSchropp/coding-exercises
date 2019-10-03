9a56ba4ff0fdb3f9a0c01d86adae96fb8d0e370f
Landon Schropp <schroppl@gmail.com>
2019-10-02 22:12:47 -0700

    Implement Bob

--- exercism/elixir/bob/lib/bob.ex
+++ exercism/elixir/bob/lib/bob.ex
@@ -1,7 +1,15 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      nothing?(input) -> "Fine. Be that way!"
+      question?(input) && yell?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      yell?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
+
+  defp question?(input), do: String.ends_with? input, "?"
+  defp yell?(input), do: String.upcase(input) == input && String.match?(input, ~r/[[:alpha:]]/)
+  defp nothing?(input), do: String.trim(input) == ""
 end
